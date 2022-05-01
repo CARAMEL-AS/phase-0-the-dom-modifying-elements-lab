869f26acf4cdce20d9a66e29f834ec96dd081ee4
Aftab Sidhu <Aftab180@gmail.com>
2022-05-01 19:05:25 -0400

    commit tree 138217c667fccb71f5cc0e7fa6cff71c54701885

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector('#main').remove()
+// document.querySelector('main').remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Aftab is the champion"
+main.append(victory)
